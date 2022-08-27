e42ffb62bb53d737b8358075d68eee8cfbc5760c
kkldream <kk69347321@gmail.com>
2022-08-27 23:42:55 +0800

    修改add方法
    add同時爬此人貼文，add的頻道不會發布其他頻道會

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const utils = require('./utils.js');
 
 const client = new Discord.Client();
 
-var timerDelay = 5000;
+var timerDelay = 10 * 60000;
 var tagId;
 
 client.on('ready', () => {
@@ -50,11 +50,58 @@ client.on('message', msg => {
                 msg.channel.send(`尚未初始化頻道 (輸入 "${tagId} init" 指令)`);
                 return;
             }
-            if (res.updateResult) {
-                msg.channel.send(`開始訂閱 ${username} 帳號 (https://www.instagram.com/${username}/)`);
+            if (!res.updateResult) {
+                msg.channel.send(`訂閱已存在 (https://www.instagram.com/${username}/)`);
                 return;
             }
-            msg.channel.send(`訂閱已存在 (https://www.instagram.com/${username}/)`);
+            msg.channel.send(`開始訂閱 ${username} 帳號 (https://www.instagram.com/${username}/)`);
+
+            // 篩選所有貼文列表取得新貼文列表
+            crawler(username)
+                // 篩選所有貼文列表取得新貼文列表
+                .then(async res => {
+                    let postIdList = [];
+                    res.forEach(post => postIdList.push(post.id));
+                    let inPosts = await db.findPostByFilter({ id: { $in: postIdList } });
+                    let inPostIdList = [];
+                    inPosts.forEach(post => inPostIdList.push(post.id));
+                    let newPosts = [];
+                    res.forEach(post => {
+                        if (inPostIdList.indexOf(post.id) === -1) newPosts.push(post);
+                    });
+                    return newPosts;
+                })
+                // 將新貼文保存至DB
+                .then(async newPosts => {
+                    console.log(`newPosts number : ${newPosts.length}`);
+                    if (newPosts.length === 0) return [];
+                    let insertResult = await db.insertPostRecord(newPosts);
+                    console.log(insertResult);
+                    return newPosts;
+                })
+                // 也將新貼文推撥至其他頻道，當前頻道不推撥
+                .then(newPosts => {
+                    var filter = {
+                        'channel.id': { $ne: msg.channel.id },
+                        'enable': true,
+                        'deleted': false,
+                    };
+                    db.getAllSubscribeChannelInfoByFilter(filter).then(res => {
+                        let findResultList = res.findResult;
+                        findResultList.forEach(findResult => {
+                            let subscribeUserList = findResult.subscribes;
+                            let subscribeChannel = new Discord.TextChannel(
+                                new Discord.Guild(client, { id: findResult.guild.id }),
+                                { id: findResult.channel.id },
+                            );
+                            newPosts.forEach(post => {
+                                if (subscribeUserList.indexOf(post.owner.username) !== -1) {
+                                    subscribeChannel.send(`${post.owner.username} 於 ${utils.timeConverter(post.postTimestamp)} 發布了新貼文\n(${post.url})`);
+                                }
+                            })
+                        })
+                    })
+                });
         });
     }
     else if (argParm[1] === 'del') {
@@ -132,7 +179,7 @@ client.on('message', msg => {
                 msg.channel.send('頻道訂閱功能目前啟動中');
                 return;
             }
-            msg.channel.send(`啟動頻道訂閱功能 (每${timerDelay / 60000}分鐘更新)`);
+            msg.channel.send(`啟動頻道訂閱功能 (每${parseInt(timerDelay / 60000)}分鐘更新)`);
         });
     }
     else if (argParm[1] === 'stop') {
@@ -155,6 +202,7 @@ client.on('message', msg => {
 });
 
 function crawlerIg() {
+    console.log('\ncrawlerIg :');
     db.getAllSubscribeChannelInfoByFilter({ 'deleted': false })
         // 取得所有訂閱者列表
         .then(res => {
@@ -173,6 +221,7 @@ function crawlerIg() {
         })
         // 透過訂閱者列表取得所有貼文列表
         .then(async allSubscribeUserList => {
+            console.log('allSubscribeUserList :');
             console.log(allSubscribeUserList);
             let allPosts = [];
             for (const subscribeUser of allSubscribeUserList) {
@@ -198,8 +247,8 @@ function crawlerIg() {
         })
         // 將新貼文推撥至各頻道並保存至DB
         .then(async newPosts => {
-            console.log(newPosts);
-            
+            console.log(`newPosts number : ${newPosts.length}`);
+
             // 推撥至各頻道
             var filter = { 'enable': true, 'deleted': false, };
             db.getAllSubscribeChannelInfoByFilter(filter).then(res => {
@@ -210,7 +259,6 @@ function crawlerIg() {
                         new Discord.Guild(client, { id: findResult.guild.id }),
                         { id: findResult.channel.id },
                     );
-                    console.log(findResult);
                     newPosts.forEach(post => {
                         if (subscribeUserList.indexOf(post.owner.username) !== -1) {
                             subscribeChannel.send(`${post.owner.username} 於 ${utils.timeConverter(post.postTimestamp)} 發布了新貼文\n(${post.url})`);
@@ -223,7 +271,7 @@ function crawlerIg() {
             if (newPosts.length === 0) return;
             let insertResult = await db.insertPostRecord(newPosts);
             console.log(insertResult);
-            
+
         });
 }
 
